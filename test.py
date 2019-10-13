d93cb542a7f4b16e006e0b68d628a611885cc7fb
ElliotRedhead <elliot.redhead@gmail.com>
2019-10-13 18:41:12 +0100

    Included model solution.

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-print("Hello world!")
+#print("Hello world!")
 
 def count_upper_case(message):
     count = 0
@@ -11,4 +11,27 @@ assert count_upper_case("") == 0, "Empty string."
 assert count_upper_case("A") == 1, "One upper case."
 assert count_upper_case("a") == 0, "One lower case."
 assert count_upper_case("£$%^") == 0, "Special characters."
-print(count_upper_case("Hello world!"))
+# print(count_upper_case("Hello world!"))
+
+def even_number_of_evens(numbers):
+    if numbers == []:
+        return False
+    else:
+        evens = 0
+    for num in numbers:
+        if num % 2 == 0:
+            evens += 1
+    if evens == 0:
+        return False
+    else:
+        return evens % 2 == 0
+
+assert even_number_of_evens([]) == False, "No numbers"
+assert even_number_of_evens([2]) == False, "One even number"
+assert even_number_of_evens([2, 4]) == True, "Two even numbers"
+assert even_number_of_evens([2, 3]) == False, "Two numbers, only one even"
+assert even_number_of_evens([2, 3, 9, 10, 13, 7, 8]) == False, "Multiple numbers, three are even"
+assert even_number_of_evens([2, 3, 9, 10, 13, 7, 8, 5, 12]) == True, "Multiple numbers, four are even"
+assert even_number_of_evens([1, 3, 9]) == False, "No even numbers"
+
+print("All tests passed!")
