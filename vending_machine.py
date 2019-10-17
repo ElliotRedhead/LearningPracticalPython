895a58898e29e60fd188bd843bf4d9de4b5b2065
unknown <elliot.redhead@gmail.com>
2019-10-17 19:47:08 +0100

    Test added for returning one coin in change.

--- vending_machine.py
+++ vending_machine.py
@@ -1,8 +1,11 @@
 from byotest import *
 
 def get_change(amount):
-    return[]
+    if amount == 0:
+        return[]
+    return[1]
 
 test_are_equal(get_change(0),[])
+test_are_equal(get_change(1),[1])
 
 print("All tests pass!")
