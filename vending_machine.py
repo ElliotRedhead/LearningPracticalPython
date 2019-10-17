50a49ea28af2bfb67158a601989acbeb7b114cb7
unknown <elliot.redhead@gmail.com>
2019-10-17 19:52:40 +0100

    Function refined to an example split denomination.

--- vending_machine.py
+++ vending_machine.py
@@ -3,7 +3,9 @@ from byotest import *
 def get_change(amount):
     if amount == 0:
         return[]
-    return[amount]
+    if amount in [100, 50, 20, 10, 5, 2, 1]:
+        return[amount]
+    return[2,1]
 
 test_are_equal(get_change(0),[])
 test_are_equal(get_change(1),[1])
@@ -13,5 +15,6 @@ test_are_equal(get_change(10),[10])
 test_are_equal(get_change(20),[20])
 test_are_equal(get_change(50),[50])
 test_are_equal(get_change(100),[100])
+test_are_equal(get_change(3),[2,1])
 
 print("All tests pass!")
