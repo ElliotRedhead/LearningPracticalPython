4e838963e7c319206a4956e5e361aa536ad5fc3c
ElliotRedhead <elliot.redhead@gmail.com>
2019-10-18 22:26:09 +0100

    Write common words to a file.

--- file_io/book.py
+++ file_io/book.py
@@ -3,4 +3,8 @@ import collections
 
 text = open('book.txt').read().lower()
 words = re.findall(r'\w+', text)
-print(collections.Counter(words).most_common(10))
+most_common_count = collections.Counter(words).most_common(10)
+most_common_count #convert this to a string first.
+f = open('mostcommonwords.txt', 'a')
+f.writelines(most_common_count)
+f.close()
