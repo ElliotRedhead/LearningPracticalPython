d5bf60f5d141dd772b15ef1bef1a2cac7381eb05
ElliotRedhead <elliot.redhead@gmail.com>
2019-10-19 21:10:52 +0100

    Common words and frequencies written to new line.

--- file_io/book.py
+++ file_io/book.py
@@ -4,7 +4,15 @@ import collections
 text = open('book.txt').read().lower()
 words = re.findall(r'\w+', text)
 most_common_count = collections.Counter(words).most_common(10)
-most_common_count #convert this to a string first.
-f = open('mostcommonwords.txt', 'a')
-f.writelines(most_common_count)
+print(most_common_count) #convert this to a string before file output.
+f = open('mostcommonwords.txt', 'w')
+
+for word in most_common_count:
+    section = word
+    for couple in section:
+        couple = str(couple) + "\n"
+        f.write(couple)
+
+# most_common_count = str(most_common_count).replace ('),', '),\n')
+# f.writelines(str(most_common_count))
 f.close()
