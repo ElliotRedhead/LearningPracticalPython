47bd495210bd4fabbea80ceb028f68dc63bad651
ElliotRedhead <elliot.redhead@gmail.com>
2019-10-20 10:03:25 +0100

    Add question function created.

--- quiz_project/quiz.py
+++ quiz_project/quiz.py
@@ -5,6 +5,17 @@ def show_menu():
     option = input("Enter option:   ")
     return option
 
+def add_question():
+    print("")
+    question = input("Enter a question\n>   ")
+    print("")
+    print("Tell me the answer")
+    answer = input("{0}\n>  ".format(question))
+    file = open("questions.txt","a")
+    file.write(question + "\n")
+    file.write(answer + "\n")
+    file.close
+
 def game_loop():
     while True:
         option = show_menu()
