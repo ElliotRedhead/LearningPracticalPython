bf8b7630b5e8af46d0f8b484008e9ce00718c0b1
Redhead <elliot.redhead@gmail.com>
2019-10-21 12:59:59 +0100

    Added neighbouring cells, tests for occupation.

--- boggle_solver/boggle_test.py
+++ boggle_solver/boggle_test.py
@@ -35,3 +35,28 @@ class TestBoggle(unittest.TestCase):
         grid = boggle.make_grid(2,3)
         for letter in grid.values():
             self.assertIn(letter,ascii_uppercase)
+    
+    def test_neighbours_of_a_position(self):
+        """
+        Ensure that a position has 8 neighbours
+        """
+        coords = (1,2)
+        neighbours = boggle.neighbours_of_position(coords)
+        self.assertIn((0,1), neighbours)
+        self.assertIn((0,2), neighbours)
+        self.assertIn((0,3), neighbours)
+        self.assertIn((1,1), neighbours)
+        self.assertIn((1,3), neighbours)
+        self.assertIn((2,1), neighbours)
+        self.assertIn((2,2), neighbours)
+        self.assertIn((2,3), neighbours)
+    
+    def test_all_grid_neighbours(self):
+        """
+        Ensure that ALL grid positions have neighbours.
+        """
+        grid = boggle.make_grid(2,2)
+        neighbours = boggle.test_all_grid_neighbours(grid)
+        selfAssertEqual(len(neighbours), len(grid))
+        for pos in grid:
+                others = list(grid)
